234664da59614603e2ebecf26b1b9b1bcd1397d7
Thorsten Scherler <scherler@gmail.com>
2022-08-23 14:50:56 +0200

    chore: [master] finishing first version
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const readline = require('readline');
 const request = require('request');
 
 const TOKEN = process.env.GIT_TOKEN
-
+// early out when we don't have a token
 if (!TOKEN) {
-    console.error('Please provide a token')
+    console.error('Please provide a git token by setting the environment variable GIT_TOKEN')
     process.exit(1);
 }
-
+// read input file
 function readFile(type) {
     return readline.createInterface({
         input: fs.createReadStream(`./plugins.${type}.txt`),
         console: false
     });
 }
-
 // generate array of plugin list
 const generateUrlList = async (type) => new Promise(
     (resolve) => {
@@ -23,26 +22,27 @@ const generateUrlList = async (type) => new Promise(
         const readInterface = readFile(type);
         readInterface.on('line', function (plugin) {
             const url = `https://api.github.com/repos/${plugin}/${type}`
-            lineBuffer.push({ plugin, url });
+            lineBuffer.push({ plugin, url, type });
         });
         readInterface.on('close', function () {
-            console.log('end');
             resolve(lineBuffer);
         });
     }
 )
-
-const main = async () => {
-    const releases = await generateUrlList('releases')
-    const tags = await generateUrlList('tags')
-    const plugins = [...releases, ...tags];
-    // generateInfo('releases');
-
-    console.log(plugins);
-};
-
-main();
-
+// wrapper function to resolve all Promise
+const getPluginsInfo = async (plugins) => new Promise(
+    (resolve, reject) => {
+        const all = plugins.map((release) => getPluginInfo(release))
+        Promise.all(all)
+            .catch((err) => { 
+                reject(err)
+            })
+            .then((plugins) => {
+                resolve(plugins);
+            })
+    }
+)
+// contact github and get releases
 const getPluginInfo = async (plugin) => new Promise(
     (resolve, reject) => {
         const lineBuffer = [];
@@ -63,13 +63,26 @@ const getPluginInfo = async (plugin) => new Promise(
         rl
             .on('line', (line) => {
                 lineBuffer.push({
-                    releases: JSON.parse(line)?.length > 0,
-                    plugin
+                    isActive: JSON.parse(line)?.length > 0,
+                    ...plugin
                 });
             })
             .on('close', () => resolve(lineBuffer));
     }
 );
+// right now we only check releases
+const main = async () => {
+    const releases = await generateUrlList('releases')
+    getPluginsInfo(releases).then((all) => {
+        fs.writeFile('./plugins.dist.json', JSON.stringify(all), err=> {
+            if (err) {
+              console.error(err);
+            }
+            console.info('file written successfully');
+            console.log(all);
+          });
+    })
+};
 
-
-
+// invoke
+main();
