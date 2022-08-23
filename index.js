bc025ed3257ecb668d2a088952d96405de780d60
Thorsten Scherler <scherler@gmail.com>
2022-08-23 13:37:15 +0200

    chore: [master] adding generating function to not go against git directly
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

--- index.js
+++ index.js
@@ -9,51 +9,67 @@ if (!TOKEN) {
     process.exit(1);
 }
 
-generateInfo('releases');
-
-function generateInfo(type) {
-    const readInterface = readline.createInterface({
+function readFile(type) {
+    return readline.createInterface({
         input: fs.createReadStream(`./plugins.${type}.txt`),
-        output: process.stdout,
         console: false
     });
-
-    readInterface.on('line', function (line) {
-        getPluginInfo(line).then((plugin) => {
-            console.log(plugin);
-        });
-    });
-
-    const getPluginInfo = async (plugin) => new Promise(
-        (resolve, reject) => {
-            const lineBuffer = [];
-            const url = `https://api.github.com/repos/${plugin}/${type}        `;
-
-            const options = {
-                url,
-                method: 'GET',
-                Authorization: `Bearer ${TOKEN}`,
-                headers: {
-                    "User-Agent": 'jenkins-plugin-checker',
-                    "Accept": 'application/vnd.github+json',
-                    "Content-Type": "application/json",
-                },
-            };
-            const callback = (error, response, body) => {
-                // console.log(body);
-                console.log(response.statusCode);
-            };
-            const rl = readline.createInterface({
-                input: request(options, callback).on('error', (err) => reject(err)),
-            });
-            rl
-                .on('line', (line) => {
-                    lineBuffer.push({
-                        releases: JSON.parse(line)?.length>0,
-                        plugin
-                    });
-                })
-                .on('close', () => resolve(lineBuffer));
-        }
-    );
 }
+
+// generate array of plugin list
+const generateUrlList = async (type) => new Promise(
+    (resolve) => {
+        const lineBuffer = [];
+        const readInterface = readFile(type);
+        readInterface.on('line', function (plugin) {
+            const url = `https://api.github.com/repos/${plugin}/${type}`
+            lineBuffer.push({ plugin, url });
+        });
+        readInterface.on('close', function () {
+            console.log('end');
+            resolve(lineBuffer);
+        });
+    }
+)
+
+const main = async () => {
+    const releases = await generateUrlList('releases')
+    const tags = await generateUrlList('tags')
+    const plugins = [...releases, ...tags];
+    // generateInfo('releases');
+
+    console.log(plugins);
+};
+
+main();
+
+const getPluginInfo = async (plugin) => new Promise(
+    (resolve, reject) => {
+        const lineBuffer = [];
+
+        const options = {
+            url: plugin.url,
+            method: 'GET',
+            Authorization: `Bearer ${TOKEN}`,
+            headers: {
+                "User-Agent": 'jenkins-plugin-checker',
+                "Accept": 'application/vnd.github+json',
+                "Content-Type": "application/json",
+            },
+        };
+        const rl = readline.createInterface({
+            input: request(options).on('error', (err) => reject(err)),
+        });
+        rl
+            .on('line', (line) => {
+                lineBuffer.push({
+                    releases: JSON.parse(line)?.length > 0,
+                    plugin
+                });
+            })
+            .on('close', () => resolve(lineBuffer));
+    }
+);
+
+
+
